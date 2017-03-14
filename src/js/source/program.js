0dc942da52cd3c6d1cbf2226a17aea7a7a38fa30
Konstantin Batrakov <k.batrakov@smileexpo.ru>
2017-03-14 12:54:16 +0300

    dell file and add new file

--- src/js/source/program.js
+++
@@ -1,7 +0,0 @@
-// Program tabs
-////////////////////////////////////////////////
-
-$('.more a').click(function () {
-	$('.wrap-more a').removeClass('active-more');
-	$(this).addClass('active-more');
-});
--- src/js/source/responsive-iframe.js
+++
@@ -1,7 +0,0 @@
-// Responsive iframe video
-
-	// Setting
-	var contentContainer = $('.open-news');
-	var videoWrapper = '<div class="embed-responsive embed-responsive-16by9"></div>';
-
-// END Responsive iframe video
